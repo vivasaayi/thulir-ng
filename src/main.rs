0bc333e23003019deb131e1fe6f544e82ffd9c71
Rajan Panneer Selvam <vivasaayi@live.com>
2023-11-05 01:20:07 -0500

    Test additional routes

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,19 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/ping")]
+fn ping() -> &'static str {
+    "Hello, world! Ping Example"
+}
+
+#[get("/health")]
+fn health() -> &'static str {
+    "Hello, world! Health Example"
+}
+
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index,ping,health])
 }
